c3b25982d7719c21c25477f5f721499a490cbedc
bethojunior <hgomescostajr@gmail.com>
2020-07-12 15:51:34 -0300

    correcao no titulo

--- public/js/modules/nav/nav.js
+++ public/js/modules/nav/nav.js
@@ -2,7 +2,7 @@ $(".button-collapse").sideNav();
 
 elementProperty.addEventInElement('#share','onclick',function () {
     navigator.share({
-        title: document.getElementById('Moobley cash').innerHTML,
+        title: 'Moobley Cash',
         text: 'Aqui está o link do site do Moobley Cash',
         url: window.location.href,
     })
