edd601d18bb1509dc91dc9747ece3f86d1711941
bethojunior <hgomescostajr@gmail.com>
2020-07-12 10:35:11 -0300

    cor do header

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,5 +1,7 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Raleway" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="theme-color" content="#2AB34B"/>
 <link href="{{ asset('assets/images/logo/icon.png') }}" rel="icon">
 <link rel="stylesheet" href="{{ asset('css/default/config.css') }}">
 <link rel="stylesheet" href="{{ asset('css/default/materialize.min.css') }}">
